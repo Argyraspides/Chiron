2b84b582159225bd7e76df821e68eefb9073c47b
Argyraspides <joshivinayak441@gmail.com>
2022-10-03 23:30:01 +1300

    Begun basic Polygon class

--- Chiron/Genesis.cpp
+++ Chiron/Genesis.cpp
@@ -2,23 +2,26 @@
 #include <SFML\Graphics.hpp>
 
 sf::RenderWindow window(sf::VideoMode(1280, 720), "Chiron");
-
+void runEngine();
+//Engine engine;
 int main()
 {
+	window.setFramerateLimit(60);
+	runEngine();
+}
 
-
+void runEngine()
+{
 	while (window.isOpen())
 	{
 		sf::Event event;
 
-		//while (window.pollEvent(event))
-			
-		window.clear(sf::Color(30, 30, 30, 255));
+		while (window.pollEvent(event))
+		{
+			//engine.run()
+		}
 
+		window.clear(sf::Color(30, 30, 30, 255));
 		window.display();
 	}
-
-
-	return (0);
-
 }
