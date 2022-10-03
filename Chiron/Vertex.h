10ef42e7b546cd6c06425072b9898333e013c07a
Argyraspides <joshivinayak441@gmail.com>
2022-10-03 22:58:47 +1300

    Added basic vector operators for Vertex struct

--- Chiron/Vertex.h
+++ Chiron/Vertex.h
@@ -1,9 +1,106 @@
 #pragma once
+#include <cmath>
 
 typedef struct Vertex {
 
-	float x, y, z = 0;
+	float x = 0;
+	float y = 0;
+	float z = 0;
 
+	Vertex operator+(const Vertex& a)
+	{
+		return 
+		{ 
+			this->x + a.x, 
+			this->y + a.y, 
+			this->z + a.z 
+		};
+	};
 
+	Vertex operator-(const Vertex& a)
+	{
+		return 
+		{ 
+			this->x - a.x, 
+			this->y - a.y, 
+			this->z - a.z 
+		};
+	};
+
+	Vertex operator*(const float& a)
+	{
+		return 
+		{ 
+			this->x * a, 
+			this->y * a, 
+			this->z * a 
+		};
+	};
+
+	Vertex operator/(const float& a)
+	{
+		return 
+		{ 
+			this->x / a, 
+			this->y / a, 
+			this->z / a 
+		};
+	};
+
+	void   operator=(const Vertex& v)
+	{
+		this->x = v.x;
+		this->y = v.y;
+	};
+
+	Vertex crossProduct(const Vertex& v2)
+	{
+		return 
+		{ 
+			v2.y * this->z - v2.z * this->y, 
+			v2.z * this->x - v2.x * this->z, 
+			v2.x * this->y - v2.y * this->x 
+		};
+	};
+
+	float dotProduct(const Vertex& v2)
+	{
+		return
+		{
+			v2.x * this->x
+			+
+			v2.y * this->y
+			+
+			v2.z * this->z
+		};
+	}
+
+	void normalize()
+	{
+		*this = 
+
+			*this
+			/ 
+			sqrt
+			(
+				this->x * this->x 
+				+ 
+				this->y * this->y 
+				+ 
+				this->z * this->z
+			);
+	};
+
+	float length()
+	{
+		return sqrt
+		(
+			this->x * this->x 
+			+ 
+			this->y * this->y 
+			+ 
+			this->z * this->z
+		);
+	};
 
 };
